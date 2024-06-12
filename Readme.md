015a9a4015d3c76f8d28eb79e69ec0197eaae3e9
Michael Foster <mifoster1723@gmail.com>
2024-06-11 20:10:48 -0500

    init readme

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# Bucket List
+Here's an application deployed on GCP to show everything I want to accomplish in life. 
+We should all strive to be the best version of ourselves. Why not begin today?
---
+++ life-goals/src/pages/goal-one.ts
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const GoalOne: React.FC = () => {
+  return(
+    <div>
+      <h1>"Goal One"</h1>
+      <p>"Praise God Everyday."</p>
+    </div>
+  );
+};
+
+export default GoalOne;
